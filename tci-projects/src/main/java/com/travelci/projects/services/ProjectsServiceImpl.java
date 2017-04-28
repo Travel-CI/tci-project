ee0228803d0ac24302d9c38d6b6e2b70900896fb
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-04-28 12:27:57 +0200

    TCI-17 Add select/update queries & adpater DTO/Entity

--- tci-projects/src/main/java/com/travelci/projects/services/ProjectsServiceImpl.java
+++ tci-projects/src/main/java/com/travelci/projects/services/ProjectsServiceImpl.java
@@ -1,25 +1,40 @@
 package com.travelci.projects.services;
 
+import com.travelci.projects.adapter.ProjectAdapter;
 import com.travelci.projects.entities.ProjectDto;
+import com.travelci.projects.entities.ProjectEntity;
+import com.travelci.projects.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class ProjectsServiceImpl implements ProjectsService {
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<ProjectDto> getProjectsList() {
-        return null;
+    private final ProjectRepository projectRepository;
+
+    private final ProjectAdapter projectAdapter;
+
+    public ProjectsServiceImpl(final ProjectRepository projectRepository, final ProjectAdapter projectAdapter) {
+        this.projectRepository = projectRepository;
+        this.projectAdapter = projectAdapter;
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public List<ProjectDto> getProjectsList() {
+        return projectRepository.findAll().stream().map(projectAdapter::toProjectDto).collect(Collectors.toList());
+    }
 
+    @Override
     public ProjectDto create(final ProjectDto projectDto) {
-        return null;
+        return projectAdapter.toProjectDto(projectRepository.save(projectAdapter.toProjectEntity(projectDto)));
     }
 
     @Override
