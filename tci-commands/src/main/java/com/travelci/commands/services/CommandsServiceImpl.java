9d551c4d2424666400bc9c8b09c0dc59e197a8a9
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-05-05 15:50:32 +0200

    TCI-18 Add Command Service Implementation w/ JpaRepository

--- tci-commands/src/main/java/com/travelci/commands/services/CommandsServiceImpl.java
+++ tci-commands/src/main/java/com/travelci/commands/services/CommandsServiceImpl.java
@@ -1,34 +1,55 @@
 package com.travelci.commands.services;
 
+import com.travelci.commands.adapter.CommandAdapter;
 import com.travelci.commands.entities.CommandDto;
+import com.travelci.commands.entities.CommandEntity;
+import com.travelci.commands.repository.CommandRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional
 public class CommandsServiceImpl implements CommandsService {
 
+    private final CommandRepository commandRepository;
+
+    private final CommandAdapter commandAdapter;
+
+    public CommandsServiceImpl(final CommandRepository commandRepository, final CommandAdapter commandAdapter) {
+        this.commandRepository = commandRepository;
+        this.commandAdapter = commandAdapter;
+    }
+
     @Override
     @Transactional(readOnly = true)
     public List<CommandDto> getCommandsByProject(final Integer projectId) {
-        return null;
+
+        List<CommandDto> commandDtoList = new ArrayList();
+        List<CommandEntity> resultQuery = commandRepository.findCommandEntitiesByProjectId(projectId);
+
+        for(CommandEntity commandEntity : resultQuery) {
+            commandDtoList.add(commandAdapter.toCommandDto(commandEntity));
+        }
+
+        return commandDtoList;
     }
 
     @Override
     public CommandDto create(final CommandDto commandDto) {
-        return null;
+        return commandAdapter.toCommandDto(commandRepository.save(commandAdapter.toCommandEntity(commandDto)));
     }
 
     @Override
     public CommandDto update(final CommandDto commandDto) {
-        return null;
+        return commandAdapter.toCommandDto(commandRepository.save(commandAdapter.toCommandEntity(commandDto)));
     }
 
     @Override
     public void delete(final CommandDto commandDto) {
-
+        commandRepository.delete(commandAdapter.toCommandEntity(commandDto));
     }
 
     @Override
