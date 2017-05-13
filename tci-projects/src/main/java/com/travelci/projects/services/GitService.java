c6c78e6cfe98c11cb4376e3dbec52c3129c51dd7
Mathieu Boisnard <m.boisnard@free.fr>
2017-05-13 17:21:25 +0200

    TCI-26 Fix Git Integration Tests for Windows

--- tci-projects/src/main/java/com/travelci/projects/services/GitService.java
+++ tci-projects/src/main/java/com/travelci/projects/services/GitService.java
@@ -2,12 +2,13 @@ package com.travelci.projects.services;
 
 import com.travelci.projects.entities.PayLoad;
 import com.travelci.projects.entities.ProjectDto;
+import org.eclipse.jgit.api.Git;
 
 import java.io.File;
 
 interface GitService {
 
-    void pullProjectRepository(ProjectDto projectDto, PayLoad webHookPayLoad);
+    Git pullProjectRepository(ProjectDto projectDto, PayLoad webHookPayLoad);
 
-    boolean deleteRepository(File repositoryFolder);
+    boolean deleteRepository(Git repository, File repositoryFolder);
 }
