77ad90ff46411231c533d779460f52f56ac134dd
Mathieu Boisnard <m.boisnard@free.fr>
2017-05-25 14:56:02 +0200

    TCI-27 Fix Rest Template Mock

--- tci-webhook/src/test/java/com/travelci/webhook/controllers/RestConfig.java
+++ tci-webhook/src/test/java/com/travelci/webhook/controllers/RestConfig.java
@@ -1,10 +1,17 @@
 package com.travelci.webhook.controllers;
 
-import org.mockito.Mockito;
+import com.travelci.webhook.entities.PayLoad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.ACCEPTED;
 
 @Configuration
 @Profile("test")
@@ -12,6 +19,12 @@ public class RestConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return Mockito.mock(RestTemplate.class);
+
+        final RestTemplate restTemplate = mock(RestTemplate.class);
+
+        when(restTemplate.postForEntity(any(String.class), any(PayLoad.class), anyObject()))
+            .thenReturn(new ResponseEntity<>(ACCEPTED));
+
+        return restTemplate;
     }
 }
