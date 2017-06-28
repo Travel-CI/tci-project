2a6acf9d7b4f81f5ca7b6af7483ed1ba071fee67
Mathieu Boisnard <m.boisnard@free.fr>
2017-06-28 18:53:49 +0200

    TCI-27 Fix database connection for tests

---
+++ tci-database/travelci.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS travelci;
+CREATE DATABASE travelci;
+
+CREATE USER tciprojects WITH PASSWORD 'tciprojects';
+CREATE USER tcicommands WITH PASSWORD 'tcicommands';
+CREATE USER tcilogger WITH PASSWORD 'tcilogger';
+
+GRANT ALL PRIVILEGES ON DATABASE travelci to tcicommands;
+GRANT ALL PRIVILEGES ON DATABASE travelci to tciprojects;
+
+DROP TABLE IF EXISTS project CASCADE;
+CREATE TABLE project (id bigserial NOT NULL, branches varchar(255), created timestamp, description varchar(255), dockerfile_location varchar(255), enable boolean, last_start timestamp, name varchar(255), repository_token varchar(255), repository_url varchar(255), updated timestamp, user_name varchar(255), user_password varchar(255), PRIMARY KEY (id));
+
+DROP TABLE IF EXISTS command CASCADE;
+CREATE TABLE command (id bigserial NOT NULL, command varchar(255), command_order int4, enable_logs boolean, enabled boolean, name varchar(255), project_id int8, PRIMARY KEY (id));
+ALTER TABLE command ADD CONSTRAINT project_id_fk FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE;
