adaaa3cc0b29a5bcd141a5bf1d695735d1170a88
Julien Bertauld <julien.bertauld@gmail.com>
2017-07-03 23:42:03 +0200

    tableau ihm projet

--- tci-webapp/src/app/project/project.component.ts
+++ tci-webapp/src/app/project/project.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {Project} from './model/project';
 
 @Component({
   templateUrl: './project.component.html'
 })
+
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  private projects: Project[];
+
+  constructor() {}
 
   ngOnInit() {
+    this.projects = [{
+      id: 25,
+      name: 'Project 1',
+      description: 'Le meilleur projet',
+      lastStart: '24/12/94'
+    },{
+      id: 26,
+      name: 'Project 2',
+      description: 'Le meilleur projet (l\'unique)',
+      lastStart: '23/11/1962'
+    },{
+      id: 29,
+      name: 'Project 45',
+      description: 'Emma la plus belle :p',
+      lastStart: '23/11/1962'
+    },{
+      id: 31,
+      name: 'Project 25',
+      description: 'julien le boss !!',
+      lastStart: '06/11/1994'
+    }];
   }
 
 }
