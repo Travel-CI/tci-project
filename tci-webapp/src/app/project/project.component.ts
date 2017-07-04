7bbff39f7a570b4f247d4e30036dc670b6e2dbe1
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-07-04 21:41:17 +0200

    TCI-31 adjustments after merge

--- tci-webapp/src/app/project/project.component.ts
+++ tci-webapp/src/app/project/project.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Project} from './model/project';
+import {Project} from './models/project';
 
 @Component({
   templateUrl: './project.component.html'
@@ -16,23 +16,9 @@ export class ProjectComponent implements OnInit {
       id: 25,
       name: 'Project 1',
       description: 'Le meilleur projet',
-      lastStart: '24/12/94'
-    },{
-      id: 26,
-      name: 'Project 2',
-      description: 'Le meilleur projet (l\'unique)',
-      lastStart: '23/11/1962'
-    },{
-      id: 29,
-      name: 'Project 45',
-      description: 'Emma la plus belle :p',
-      lastStart: '23/11/1962'
-    },{
-      id: 31,
-      name: 'Project 25',
-      description: 'julien le boss !!',
-      lastStart: '06/11/1994'
+      enable: true,
+      repositoryUrl: 'https://github.com/mboisnard/test-repo',
+      branches: [ "master", "dev"]
     }];
   }
-
 }
