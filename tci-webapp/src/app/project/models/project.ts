5b47a3f1fec215016eb1f1f643b743c2d2ec8314
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-07-06 10:27:53 +0200

    TCI-33 Complete Project Object Mapping between Back & Front

--- tci-webapp/src/app/project/models/project.ts
+++ tci-webapp/src/app/project/models/project.ts
@@ -5,4 +5,12 @@ export interface Project {
   enable: boolean;
   repositoryUrl: string;
   branches: [string];
+  userName: [string];
+  userPassword: [string];
+  repositoryToken: [string];
+  dockerfileLocation: [string];
+  lastStart: [Date];
+  created: [number];
+  updated: [number];
+
 }
