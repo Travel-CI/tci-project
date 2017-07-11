ea6509e44f4e022a4d8dd922eadeb02668276d7e
Mathieu Boisnard <m.boisnard@free.fr>
2017-07-11 13:38:29 +0200

    TCI-49 Delete 1 Build & Change Build List Order

--- tci-webapp/src/app/services/logger.service.ts
+++ tci-webapp/src/app/services/logger.service.ts
@@ -10,10 +10,21 @@ export class LoggerService {
   constructor(private http: Http) { }
 
   getAllBuildsForProject(id: number) : Promise<Build[]> {
-  return this.http.get('/api/builds/' + id)
-    .toPromise()
-    .then((res: Response) => res.json() as Build[])
-    .catch((err: Error) => this.handleError(err));
+    return this.http.get('/api/builds/' + id)
+      .toPromise()
+      .then((res: Response) => res.json() as Build[])
+      .catch((err: Error) => this.handleError(err));
+  }
+
+  deleteBuildForProject(projectId: number, buildId: number): Promise<number> {
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.delete('/api/builds/' + projectId + '/' + buildId, options)
+      .toPromise()
+      .then((res: Response) => res.json() as number)
+      .catch((err: Error) => this.handleError(err));
   }
 
   handleError(err: Error): Promise<Build[]> {
