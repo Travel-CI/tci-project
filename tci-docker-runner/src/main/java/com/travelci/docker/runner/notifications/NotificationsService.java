60c49cb0560a14bbb4399ed09245035b196b2df7
Julien Bertauld <julien.bertauld@gmail.com>
2017-07-16 14:04:16 +0200

    TCI-52 implementation in docker runner - Add Notifications in commands - Implementation Notifications in docker runner

---
+++ tci-docker-runner/src/main/java/com/travelci/docker/runner/notifications/NotificationsService.java
@@ -0,0 +1,10 @@
+package com.travelci.docker.runner.notifications;
+
+import com.travelci.docker.runner.notifications.entities.Email;
+import com.travelci.docker.runner.project.entities.ProjectDto;
+
+public interface NotificationsService {
+    void sendSuccessNotification(ProjectDto project);
+    void sendErrorNotification(ProjectDto project);
+    void sendNotification(final Email email);
+}
